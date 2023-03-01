550951b602a8a7fa0e081336e5e5393b9a6390cb
Sushmayana Mishra <sushmayanamishra@Sushmayanas-MacBook-Pro.local>
2023-03-01 13:15:04 +0530

    new project

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const App= () => {
+    const[todo,setTodo]= useState("");
+    const[todos,setTodos]=useState(0);
+    if(todo!=="") {
+        const addTodo = () => {
+            setTodos([...todos, todo]);
+            setTodo("");
+        }
+    }
+    const deleteTodo = () =>{
+        const newTodo = todos.filter((todo)=>{
+            return todo !== text;
+
+        });
+        setTodos(newTodo);
+    };
+
+
+
+    return (
+        <div className="App">
+            <h1> React Todo App</h1>
+            <div className="input-wrapper">
+                <input type="text" name="todo" placeholder="create a new todo"
+                onChange={(e)=> setTodo(e.target.value)}}/>
+                <button className="add-button">Add</button>
+                <ul className="todo-list">{todos.map((todo,index)=>(
+                <div className="todo1">
+                    <li> key={index}>{todo} </li>
+                    <button className="delete-button" onClick={deleteTodo(todo)}>Delete</button>
+
+                </div>))}
+                </ul>
+            </div>
+
+        </div>
+    );
+}
