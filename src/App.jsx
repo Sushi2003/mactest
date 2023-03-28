45fd6f788090027523274944157e1c5b70f82621
Sushmayana Mishra <sushmayanamishra@Sushmayanas-MacBook-Pro.local>
2023-03-28 12:13:14 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-const App= () => {
-    const[todo,setTodo]= useState("");
-    const[todos,setTodos]=useState(0);
-    if(todo!=="") {
-        const addTodo = () => {
-            setTodos([...todos, todo]);
-            setTodo("");
-        }
+import {useState} from "react";
+const [todo,setTodo]=useState("");
+const[todos,setTodos]=useState(0);
+//program to add all the todos entered
+const addTodo = ()=>{
+    if(todo!== " "){
+        setTodos([...todos,todo]);
+        setTodo("");
     }
-    const deleteTodo = () =>{
-        const newTodo = todos.filter((todo)=>{
-            return todo !== text;
-
-        });
-        setTodos(newTodo);
-    };
-
-
-
-    return (
-        <div className="App">
-            <h1> React Todo App</h1>
-            <div className="input-wrapper">
-                <input type="text" name="todo" placeholder="create a new todo"
-                onChange={(e)=> setTodo(e.target.value)}/>
-                <button className="add-button">Add</button>
-                <ul className="todo-list">{todos.map((todo,index)=>(
-                <div className="todo1">
-                    <li key={index}> {todo} </li>
-                    <button className="delete-button" onClick={deleteTodo(todo)}>Delete</button>
-                </div>))}
-                </ul>
-            </div>
-
-        </div>
+}
+//creating a delete function
+const deleteTodo=(text)=>{
+    const newTodo= todos.filter((todo)=>{
+        return todo!=text;
+    }
     );
+    setTodos(newTodo);
 }
 
-export default App;
+
+
+
+
+
+
+
+//creating a initial mockup,
+const App=()=>{
+    return <div>
+        <h1>
+            React todo App
+        </h1>
+        <div>
+            <input type="text" name="todo" placeholder="Create a new todo" onChange={(e)=>{setTodo(e.target.value)}}></input>
+            <button onClick={addTodo}>Add</button>
+        </div>
+        <div>
+            <ul className="todo-list">
+                <li key={index}>{todo}</li>
+                <button className="delete-todo" onClick={deleteTodo}>Delete</button>
+            </ul>
+        </div>
+    </div>
+}
--- src/components/svg-editor.jsx
+++ src/components/svg-editor.jsx
@@ -1,57 +1,106 @@
-import React, {useRef, useEffect, useState} from 'react';
-import {carSvg} from "./svg-fixture.js";
+// import React, {useRef, useEffect, useState} from 'react';
+// import {carSvg} from "./svg-fixture.js";
+//
+// console.log('carSvg', carSvg);
+//
+// // parse the svg
+//
+//
+// function SVGEditor() {
+//
+//   // let counter = 0;
+//   const [counter, setCounter] = useState(0);
+//
+//   const ref = useRef(); // ignore this part
+//
+//   useEffect(() => {
+//     if (ref.current) {
+//       const parser = new DOMParser();
+//       const doc = parser.parseFromString(carSvg, 'image/svg+xml');
+//       const svgNode = doc.firstElementChild;
+//       console.log('svgNode',svgNode);
+//
+//       const div = ref.current;
+//       div.appendChild(svgNode);
+//     }
+//   }, [ref]);
+//
+//   useEffect(() => {
+//     console.log('running useEffect for counter ', counter);
+//   }, [counter]);
+//
+//
+//   const handleClick = () => {
+//     setCounter(counter + 1);
+//     // console.log('counter is ', counter);
+//   }
+//
+//     return (
+//         <div>
+//           <h1>SVG Editor</h1>
+//           <div ref={ref}>
+//             This is the div element.
+//           </div>
+//
+//           <div>
+//             Value of the counter is {counter}
+//           </div>
+//
+//           <button onClick={handleClick}>
+//             Increment
+//           </button>
+//
+//         </div>
+//     );
+// }
+//
+// export default SVGEditor;
 
-console.log('carSvg', carSvg);
 
-// parse the svg
-
-
-function SVGEditor() {
-
-  // let counter = 0;
-  const [counter, setCounter] = useState(0);
-
-  const ref = useRef(); // ignore this part
-
-  useEffect(() => {
-    if (ref.current) {
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(carSvg, 'image/svg+xml');
-      const svgNode = doc.firstElementChild;
-      console.log('svgNode',svgNode);
-
-      const div = ref.current;
-      div.appendChild(svgNode);
-    }
-  }, [ref]);
-
-  useEffect(() => {
-    console.log('running useEffect for counter ', counter);
-  }, [counter]);
-
-
-  const handleClick = () => {
-    setCounter(counter + 1);
-    // console.log('counter is ', counter);
-  }
-
-    return (
-        <div>
-          <h1>SVG Editor</h1>
-          <div ref={ref}>
-            This is the div element.
-          </div>
-
-          <div>
-            Value of the counter is {counter}
-          </div>
-
-          <button onClick={handleClick}>
-            Increment
-          </button>
-
-        </div>
-    );
+//using useState
+import {useState} from "react";
+const [todo,setTodo]= useState("");
+//adding a item to todos list now
+const [todos,setTodos]= useState([]);
+ const addTodo = () => {
+     if(todo !== " ") {
+         setTodos([...todos, todo]);
+         setTodo(""); //turning the value as null after adding
+     }
+ };
+//deleting todo items
+const deleteTodo = (text)=> {
+    const newTodos = todos.filter((todo)=>{
+        return todo != text;
+    });
+    setTodos(newTodos);
 }
 
-export default SVGEditor;
+
+
+const App = ()=> {
+    return <div className='App'>
+        <h1>
+            React Todo App
+        </h1>
+
+        <div className='inputwrapper'>
+            <input type="text" name="todo" placeholder="create a new todo"
+            onChange={(e) =>{
+            setTodo(e.target.value)}// gets the value to getter method
+            }/>
+            <button className="add-button" onClick={addTodo}>Add </button>
+        </div>
+        {/*displaying the todos item */}
+        <ul className="todo-list">
+            {todos.map((todo,index)=> (
+                <div className="todo">
+                    <li key={index}>{todo}</li>
+                    <button className="delete-button" onClick={ () =>deleteTodo(todo)}>Delete</button>
+                </div>
+                ))}
+        </ul>
+    </div>
+};
+export default App;
+//output not produced
